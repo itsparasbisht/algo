9873aff74a4aaab2be894a04b6ddeeff469299a2
itsparasbisht <iamparasbisht@gmail.com>
2022-06-14 20:35:33 +0530

    powerOf2

---
+++ powerOf2.js
@@ -0,0 +1,31 @@
+function isPowerOf2(n) {
+  if (n < 1) {
+    return false;
+  }
+  while (n > 1) {
+    if (n % 2 !== 0) {
+      return false;
+    }
+    n = n % 2;
+  }
+  return true;
+}
+
+console.log(isPowerOf2(1));
+console.log(isPowerOf2(2));
+console.log(isPowerOf2(5));
+
+// Big O = O(log n)
+
+function isPowerOf2Bitwise(n) {
+  if (n < 1) {
+    return false;
+  }
+  return (n & (n - 1)) === 0;
+}
+
+console.log(isPowerOf2Bitwise(1));
+console.log(isPowerOf2Bitwise(2));
+console.log(isPowerOf2Bitwise(5));
+
+// Big O = O(1)
